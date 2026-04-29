743446766b97122e0ca56198d5349cbdce700ed2
Andrew Sazonov <andrew.sazonov@icloud.com>
2026-04-29 15:42:25 +0200

    Apply templates

---
+++ tests/functional/test_dummy.py
@@ -0,0 +1,7 @@
+# SPDX-FileCopyrightText: 2021-2026 EasyApplication contributors <https://github.com/easyscience>
+# SPDX-License-Identifier: BSD-3-Clause
+
+def test_dummy():
+    calculated = 2 + 2
+    expected = 4
+    assert calculated == expected
